927967d542f72b8f3127a1119ca38a0eaae2f750
Emanuel Maia <tavanomanu@gmail.com>
2022-09-28 15:20:03 +0100

    Added moving X char

--- src/main/java/com/emanuelmaia/hero/Game.java
+++ src/main/java/com/emanuelmaia/hero/Game.java
@@ -2,17 +2,20 @@ package com.emanuelmaia.hero;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
-import java.nio.IntBuffer;
+import java.security.Key;
 
 public class Game {
     public Screen screen;
-
+    private int x = 10;
+    private int y = 10;
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(40, 20);
@@ -32,11 +35,44 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
+    private boolean processKey(com.googlecode.lanterna.input.KeyStroke key) throws IOException {
+        System.out.println(key);
+
+        if(key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+            screen.close();
+        }
+
+        if(key.getKeyType() == KeyType.EOF) {
+            return false;
+        }
+
+        switch(key.getKeyType()) {
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+        }
+
+        return true;
+    }
+
     public void run() throws IOException {
-        draw();
+        com.googlecode.lanterna.input.KeyStroke key;
+        do {
+            draw();
+            key = screen.readInput();
+        } while (processKey(key));
     }
 }
