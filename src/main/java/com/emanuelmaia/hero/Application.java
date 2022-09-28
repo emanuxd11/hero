c5040422368ed6d6c5c8619d7d9af8d2414a4475
Emanuel Maia <tavanomanu@gmail.com>
2022-09-28 10:03:27 +0100

    Added Lanterna

--- src/main/java/com/emanuelmaia/hero/Application.java
+++ src/main/java/com/emanuelmaia/hero/Application.java
@@ -1,7 +1,24 @@
 package com.emanuelmaia.hero;
 
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            Screen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null); // we don't need a cursor
+            screen.startScreen(); // screens must be started
+            screen.doResizeIfNecessary(); // resize screen if necessary
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
